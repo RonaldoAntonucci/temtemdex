569f6ad3208b512fca0c79787311aabf3a4c18e1
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-12-27 01:29:23 -0300

    memo temtem card

--- src/pages/Home/TemtemCard/index.tsx
+++ src/pages/Home/TemtemCard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Animated } from 'react-native';
 
 import { Temtem } from '../../../types';
@@ -56,4 +56,4 @@ const TemtemCard: React.FC<TemtemCardProps> = ({
   );
 };
 
-export default TemtemCard;
+export default memo(TemtemCard);
